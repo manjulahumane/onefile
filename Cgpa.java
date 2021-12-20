da767bea6217542faf6a0e2cb502422abf26b777
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-20 16:44:01 +0530

    file

---
+++ Cgpa.java
@@ -0,0 +1,16 @@
+class Cgpa{
+static double[]agg={1.11,2.1,3.1,4.7,5.3,6.4,7.3,8.4,9.8,1.13};
+public static void main(String[] args){
+System.out.println(agg[0]);
+System.out.println(agg[1]);
+System.out.println(agg[3]);
+System.out.println(agg[4]);
+System.out.println(agg[5]);
+System.out.println(agg[6]);
+System.out.println(agg[7]);
+System.out.println(agg[8]);
+System.out.println(agg[9]);
+
+}
+
+}
