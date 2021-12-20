4b88e10b50bedb25d60ca83481378e1d84f66607
manjulahumane <manjulab.xworkz@gmail.com>
2021-12-20 16:53:40 +0530

    load file

---
+++ Yes.java
@@ -0,0 +1,15 @@
+class Yes{
+static boolean[]really={true,false,false,false,true,true,true,false,false,true};
+public static void main(String[] args){
+System.out.println(really[0]);
+System.out.println(really[1]);
+System.out.println(really[3]);
+System.out.println(really[4]);
+System.out.println(really[5]);
+System.out.println(really[6]);
+System.out.println(really[7]);
+System.out.println(really[8]);
+System.out.println(really[9]);
+
+}
+}
